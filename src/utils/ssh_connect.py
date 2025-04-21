975db6e89c3a7a872c87401cd1dd486eefb6b642
votaon <votaon@seznam.cz>
2025-04-21 17:45:02 +0200

    Kdo ví

--- src/utils/ssh_connect.py
+++ src/utils/ssh_connect.py
@@ -4,7 +4,7 @@ import os
 def ssh_connect():
     username = os.getenv("SFTP_USERNAME")
     password = os.getenv("SFTP_PASSWORD")
-    ip = os.getenv("DB_HOST")
+    ip = os.getenv("SFTP_HOST")
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
