ff28a7ed547ccab9037865984f81caae39197a0c
jan-holecek <honza.holecek2@gmail.com>
2025-10-12 18:20:18 +0200

    Small format changes

--- src/route/auth_bp.py
+++ src/route/auth_bp.py
@@ -1,6 +1,6 @@
 import flask_login
 from sqlalchemy import or_
-from flask import Blueprint, request
+from flask import Blueprint, make_response, request
 from werkzeug.security import check_password_hash
 from src.utils.response import send_response
 from src.models.User import User
@@ -31,7 +31,9 @@ def login():
 @flask_login.login_required
 def logout():
     flask_login.logout_user()
+
     return send_response(200, 7011, {"message": "Logged out"}, "success")
+
 
 @auth_bp.route("/auth/verify", methods=["GET"])
 def verifyUser():
