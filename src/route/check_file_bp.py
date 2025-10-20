e01f2bd59af53d386f3cecfa141ae8dac78ba28f
votaon <votaon@seznam.cz>
2025-10-20 18:15:02 +0200

    Checking access to files works

--- src/route/check_file_bp.py
+++ src/route/check_file_bp.py
@@ -18,8 +18,8 @@ hmac_secret = os.getenv("HMAC_SECRET")
 expires_in = 600
 
 @check_file_bp.route("/file/pfp/<string:filename>", methods = ["GET"])
-@check_file_access("profilePictures")
 @flask_login.login_required
+@check_file_access("profilePictures")
 def generate_pfp_token(filename, id, id2, type):
     message = pfp_path + filename
     expiry_timestamp = int(time.time()) + expires_in
@@ -40,43 +40,45 @@ def generate_pfp_token(filename, id, id2, type):
 
 
 @check_file_bp.route("/file/tasks/<string:id>/<string:id2>/<string:type>/<string:filename>", methods = ["GET"])
-@check_file_access("tasks")
 @flask_login.login_required
+@check_file_access("tasks")
 def check_tasks(filename, id, id2, type):
     message = task_path + id + id2 + type + filename
 
     expiry_timestamp = int(time.time()) + expires_in
-
     payload = f"{message}:{expiry_timestamp}"
-    token_signature = hmac.new(
-        str(hmac_secret).encode(),
+
+    sig = hmac.new(
+        hmac_secret.encode(),
         payload.encode(),
         hashlib.sha256
     ).digest()
 
-    token_data = f"{message}:{expiry_timestamp}".encode() + b":" + token_signature
-    token = base64.urlsafe_b64encode(token_data).decode()
+    sig_hex = sig.hex()
+    token_str = f"{payload}:{sig_hex}"
+    token = base64.urlsafe_b64encode(token_str.encode()).decode().rstrip("=")
 
     redirect_url = f"{ip}{task_path}{id}{id2}{type}{quote(filename)}?token={quote(token)}"
     return redirect(redirect_url)
 
 @check_file_bp.route("/file/task/<string:id>/<string:filename>", methods = ["GET"])
-@check_file_access("task")
 @flask_login.login_required
+@check_file_access("task")
 def check_task(filename, id, id2, type):
     message = task_path + id + filename
 
     expiry_timestamp = int(time.time()) + expires_in
-
     payload = f"{message}:{expiry_timestamp}"
-    token_signature = hmac.new(
-        str(hmac_secret).encode(),
+
+    sig = hmac.new(
+        hmac_secret.encode(),
         payload.encode(),
         hashlib.sha256
     ).digest()
 
-    token_data = f"{message}:{expiry_timestamp}".encode() + b":" + token_signature
-    token = base64.urlsafe_b64encode(token_data).decode()
+    sig_hex = sig.hex()
+    token_str = f"{payload}:{sig_hex}"
+    token = base64.urlsafe_b64encode(token_str.encode()).decode().rstrip("=")
 
     redirect_url = f"{ip}{task_path}{id}/{quote(filename)}?token={quote(token)}"
     return redirect(redirect_url)
